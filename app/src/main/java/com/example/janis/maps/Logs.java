e18a5e4c2c38006ac64632ff3f6649b8098b0536
Janis Christodulow <janis.christodulow@shinepowered.com>
2018-09-25 15:04:28 +0200

    overlay working

--- app/src/main/java/com/example/janis/maps/Logs.java
+++ app/src/main/java/com/example/janis/maps/Logs.java
@@ -4,17 +4,19 @@ import android.app.Activity;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class Logs {
     private Activity activity;
     public Logs (Activity activity) {
         this.activity = activity;
     }
 
-    public void log(String tag, Object content) {
+    public void log(String tag, Object... content) {
         if(BuildConfig.DEBUG) {
-            Toast.makeText(activity, String.valueOf(content), Toast.LENGTH_LONG).show();
+            Toast.makeText(activity, String.valueOf(Arrays.toString(content)), Toast.LENGTH_LONG).show();
         }
 
-        Log.d(tag, String.valueOf(content));
+        Log.d(tag, String.valueOf(Arrays.toString(content)));
     }
 }
--- app/src/main/java/com/example/janis/maps/MapsActivity.java
+++ app/src/main/java/com/example/janis/maps/MapsActivity.java
@@ -3,66 +3,295 @@ package com.example.janis.maps;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.tasks.Task;
 
-import java.security.Permission;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int COLOR_BLACK_ARGB = 0xdd000000;
+    final int REQUEST_CHECK_SETTINGS = 1;
+    final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2;
 
+    private List<List<LatLng>> tiles = new ArrayList<>();
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
     private GoogleMap mMap;
     private Logs logs;
     private FloatingActionButton fab1;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private PolygonOptions plo;
+    private Polygon polygon1 = null;
+    private double maxLat = 85.1054596961173;
 
-    LocationManager mLocationManager;
-
-    double longitude;
-    double latitude;
+    private ArrayList<Location> locationArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_maps);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         logs = new Logs(this);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        // Initialize map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-        fab1 = findViewById(R.id.fab_01);
-
-        fab1.setOnClickListener(v -> Toast.makeText(MapsActivity.this, "Test", Toast.LENGTH_LONG).show());
-
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        } else {
-            setUp();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
 
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-                0, mLocationListener);
+        createLocationRequest();
 
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    boolean minDistance = true;
+                    int counter = 0;
+                    for (Location loc : locationArray) {
+                        logs.log("locationArray", loc.getLongitude(), loc.getLatitude() + counter++ + '\n');
+                        if (!isMinDistance(loc, location)) {
+                            minDistance = false;
+                        }
+                    }
+                    if (minDistance) {
+                        locationArray.add(location);
+                        update();
+                    }
+                }
+            }
+
+            ;
+        };
+
+        fab1 = findViewById(R.id.fab_01);
+        fab1.setOnClickListener(this::onClickFab);
+
+    }
+
+    public boolean isMinDistance(Location o, Location n) {
+        return o.getLatitude() - n.getLatitude() >= 0.0005 + 0.00100005 ||
+                o.getLatitude() + n.getLatitude() >= 0.0005 + 0.00100005 ||
+                o.getLongitude() + n.getLongitude() >= 0.001 + 0.00100005 ||
+                o.getLongitude() - n.getLongitude() >= 0.001 + 0.00100005;
+    }
+
+    public ArrayList<LatLng> getTile(Location location) {
+        ArrayList<LatLng> tile = new ArrayList<>();
+        double lat = calcCoord(location.getLatitude(), 0.001);
+        double lng = calcCoord(location.getLongitude(), 0.002);
+        double diff = 0.0000005 / 2;
+
+        tile.add(new LatLng(lat + diff, lng + diff));
+        tile.add(new LatLng(lat + 0.001 - diff, lng));
+        tile.add(new LatLng(lat + 0.001 - diff, lng + 0.002 - diff));
+        tile.add(new LatLng(lat, lng + 0.002 - diff));
+        return tile;
+    }
+
+    public double calcCoord(double coordinate, double tileSize) {
+        if(coordinate<0)
+            return (((int) (coordinate / tileSize)) * tileSize)-tileSize;
+        else
+            return ((int) (coordinate / tileSize)) * tileSize;
+    }
+
+    public void update() {
+        mMap.clear();
+
+        plo = new PolygonOptions()
+                .fillColor(COLOR_BLACK_ARGB)
+                .add(new LatLng(maxLat, -180),
+                        new LatLng(maxLat, -60),
+                        new LatLng(maxLat, 0),
+                        new LatLng(maxLat, 60),
+                        new LatLng(maxLat, 179.9999999999999),
+                        new LatLng(-maxLat, 179.9999999999999),
+                        new LatLng(-maxLat, 60),
+                        new LatLng(-maxLat, 0),
+                        new LatLng(-maxLat, -60),
+                        new LatLng(-maxLat, -180));
+
+        for (int i = 0; i < locationArray.size(); i++) {
+            ArrayList<LatLng> tile = getTile(locationArray.get(i));
+            if(!tiles.contains(tile))
+                tiles.add(tile);
+        }
+
+        for(List<LatLng> tile:tiles){
+            plo.addHole(tile);
+        }
+
+        polygon1 = mMap.addPolygon(plo);
+        polygon1.setTag("Overlay");
+        polygon1.setStrokeColor(Color.TRANSPARENT);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // put your code here...
+        createLocationRequest();
+        startLocationUpdates();
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    mMap.setMyLocationEnabled(true);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, locationSettingsResponse -> {
+            logs.log("task", "success");
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+            MapsActivity.this.mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(MapsActivity.this, location -> {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            boolean minDistance = true;
+                            for (Location loc : locationArray) {
+                                if (!isMinDistance(loc, location)) {
+                                    minDistance = false;
+                                }
+                            }
+                            if (minDistance) {
+                                locationArray.add(location);
+                            }
+                            update();
+                        }
+                    });
+
+        });
+
+        task.addOnFailureListener(this, e -> {
+            logs.log("task", "failure");
+            if (e instanceof ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    resolvable.startResolutionForResult(MapsActivity.this,
+                            REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException sendEx) {
+                    // Ignore the error.
+                }
+            }
+        });
+    }
+
+    protected void onClickFab(View v) {
+        update();
+        ((FloatingActionButton) v).setSupportBackgroundTintList(ColorStateList.valueOf(changeHue(getColor(((FloatingActionButton) v)), 1)));
+    }
+
+    private int changeHue(@ColorInt int color, int rotate) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(color, hsv);
+        hsv[0] += (hsv[0] + rotate) > 360 ? rotate - 360 : rotate;
+        return Color.HSVToColor(hsv);
+    }
+
+    private int getColor(FloatingActionButton v) {
+        return Objects.requireNonNull(v.getSupportBackgroundTintList()).getDefaultColor();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null);
     }
 
     @Override
@@ -70,49 +299,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    setUp();
-                }
+                setUp();
             }
         }
     }
 
+    @SuppressLint("MissingPermission")
     private void setUp() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-
-            Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
-            logs.log("SETUP", "setup");
-        }
     }
-
-    private final LocationListener mLocationListener = new LocationListener() {
-        @SuppressLint("MissingPermission")
-        @Override
-        public void onLocationChanged(final Location location) {
-
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-
-
-
 
     /**
      * Manipulates the map once available.
@@ -123,12 +317,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        mMap.setMyLocationEnabled(true);
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
 }
